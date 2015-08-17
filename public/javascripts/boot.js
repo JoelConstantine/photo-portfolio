71d39792cab579baa6a29efacaed4c4fac65e4ba
Joel Constantine <joel@joelconstantine.com>
2015-08-17 14:57:00 -0500

    Added new app files

--- public/javascripts/boot.js
+++ public/javascripts/boot.js
@@ -1,6 +1,14 @@
 head.load(
-	{ file: 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'},
+	{ file: 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.js'},
+	{ file: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'},
 	{ file: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular.js' },
 	{ file: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular-route.js'},
+	{ file: 'portfolio/app/App.js'},
+	{ file: 'portfolio/app/models/NavigationModel.js'},
+	{ file: 'portfolio/app/services/EndpointService.js'},
+
+	{ file: 'portfolio/navigation/navigation.js'},
+	{ file: 'portfolio/navigation/controllers/NavigationController.js'},
+
 	{ file: 'portfolio/portfolio.js' }
 );
