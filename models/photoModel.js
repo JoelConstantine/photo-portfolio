3ad1a21bf1cfede7d773cc65928e90a5fbd3b8dc
Joel Constantine <joel@joelconstantine.com>
2015-08-17 11:59:25 -0500

    Can retrieve all photos based on portfolio

--- models/photoModel.js
+++ models/photoModel.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var schemas = require('../schemas.js');
+var db_table = 'mediafile'; 
 
 var Photo = function(data) {
 	this.data = data;
@@ -11,25 +13,35 @@ Photo.getAll = function(req) {
 	return photos.find({},{});
 }
 
-Photo.findByID = function(id) {
+Photo.findByID = function(req) {
 	var db = req.db;
 	var photos = db.get('mediafile');
+	var id = req.params.id;
+
 	return photos.find({'_id': ''})
 }
 
+Photo.findByPortfolio = function(req, portfolio) {
+	var db = req.db;
+	var photos = db.get(db_table);
+	query = { '_id' : { $in : portfolio.photos }}
+	console.log(portfolio.photos);
+	return photos.find({_id: {$in: portfolio.photos }})
+}
+
 Photo.deleteByID = function(req,id) {
-	
+	var db = req.db;
+	var photos = db.get(db_table);
+
+	return photos.remove({ '_id' : photoID });
 }
 
 Photo.prototype = {
 	data: {},
-	getByID: function(id) {
+	save: function(params) {
 
 	},
-	addNew: function(params) {
-
-	},
-	deleteByID: function(id) {
+	remove: function(id) {
 
 	}
 }
