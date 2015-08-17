7e42b77dd01b07d3a01b921d1b69d3a70175f97f
Joel Constantine <joel@joelconstantine.com>
2015-08-17 12:00:56 -0500

    Added basic schemas

---
+++ schemas.js
@@ -0,0 +1,16 @@
+schemas = {
+	portfolios: {
+		id: null,
+		title: null,
+		slug: null,
+		photos: {}
+	},
+	photos: {
+		id: null,
+		title: null,
+		url: null,
+		height: null,
+		width: null,
+		caption: null
+	}
+}
