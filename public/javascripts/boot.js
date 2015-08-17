aa5d6efd3a76848f7db135f18529efdf479022db
Joel Constantine <joel@joelconstantine.com>
2015-08-17 13:12:57 -0500

    Renamed app to boot

---
+++ public/javascripts/boot.js
@@ -0,0 +1,6 @@
+head.load(
+	{ file: 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'},
+	{ file: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular.js' },
+	{ file: 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.3/angular-route.js'},
+	{ file: 'portfolio/portfolio.js' }
+);
