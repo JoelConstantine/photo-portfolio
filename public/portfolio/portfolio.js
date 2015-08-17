493038165bd1ce8e5fdfd70aa7b72b4446420edd
Joel Constantine <joel@joelconstantine.com>
2015-08-17 18:29:10 -0500

    Added routes to Portfolios

--- public/portfolio/portfolio.js
+++ public/portfolio/portfolio.js
@@ -1,7 +1,8 @@
 var portfolio = angular.module('Portfolio', [
 		'ngRoute',
 		'Portfolio.Common',
-		'Portfolio.Navigation'
+		'Portfolio.Navigation',
+		'Portfolio.Portfolios'
 	]);
 
 portfolio.config(function($routeProvider) {
@@ -9,6 +10,14 @@ portfolio.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'portfolio/homepage/temp/homepage.html'
+		})
+		.when('/portfolios', {
+			templateUrl: 'portfolio/portfolios/temp/portfolio.html',
+			controller: 'PortfoliosController',
+			controllerAs: 'portfolios'
+		})
+		.when('/portfolios/:slug', {
+			templateUrl: '/portfolio/portfolios/templ/single-portfolio.html'
 		})
 
 })
